6e58eb0c55e1f2fbcaf756f6e815db11bf9777a8
Jason Miller <jmillerlaureate@gmail.com>
2014-09-26 09:13:18 -0400

    .

---
+++ R/hackR_functions.R
@@ -0,0 +1,6 @@
+## File: hackR_functions.R
+## Description: Functions called by hackR-package.R
+## Copyright: (c) 2014, Jason D. Miller, MS, MS
+## http://hack-r.com 
+## http://hack-r.github.io
+## http://github.com/hack-r
